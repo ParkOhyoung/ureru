10a945155bc04f34bc3259902eab944a972bbfe0
parkohyoung <ohyoung.park@icloud.com>
2016-05-24 21:39:39 +0900

    issue-4 extract helper functions

--- ureru/__init__.py
+++ ureru/__init__.py
@@ -3,61 +3,21 @@ import argparse
 from bisect import bisect_left
 from collections import defaultdict, namedtuple
 from functools import partial
-import io
 import itertools
-import json
 from operator import itemgetter
-import re
 import sys
 
 from bs4 import BeautifulSoup, SoupStrainer
+
+from helpers import change_tag_for_bs4, ignore_some_stuff, make_sample
 
 VERSION = sys.version_info.major
 if VERSION == 2:
     map = itertools.imap
     zip = itertools.izip
 
-
-_change_tag_for_bs4 = lambda s: re.sub(r'(</*)(sync)',
-                                       r'\1td',
-                                       s,
-                                       flags=re.I)
-_ignore_some_stuff = lambda s: re.sub(r'(\s*\-+\s*|<br\s*\/*>|\&nbsp;*)',
-                                      '\n',
-                                      s,
-                                      flags=re.I)
 Caption = namedtuple('Caption', 'since, until, sentence')
 Caption.__new__ = partial(Caption.__new__, until=None)
-
-
-def make_sample(file_name, data):
-    with io.open('../utils/{}.js'.format(file_name), 'w', encoding='utf8') as js_file:
-        data = json.dumps(to_dict_for_highchart(data), ensure_ascii=False)
-        js_file.write(u'var {}_data='.format(file_name))
-        js_file.write(data)
-        js_file.write(u';')
-
-
-def to_dict_for_highchart(time_series):
-    def to_dict(accu, caption):
-        return {
-            'x': caption.since,
-            'y': accu,
-            'z': caption.until,
-            'c': caption.sentence.replace('\n', '<br>')
-        }
-    results = []
-    for key, captions in time_series.items():
-        c1, c2 = itertools.tee(captions)
-        accumulates = itertools.accumulate((c.since for c in c1))
-        tmp_dictionary = {'name': key}
-        tmp_dictionary['data'] = [to_dict(accu, caption) for (accu, caption) in zip(accumulates, c2)]
-        results.append(tmp_dictionary)
-    return results
-
-
-def to_string(caption):
-    return '{c.since}>{c.until} : {c.sentence}\n\n'.format(c=caption)
 
 
 def make_time_series(sami):
@@ -125,8 +85,8 @@ def make_time_series(sami):
 
 
 def salt(sami):
-    like_html = _change_tag_for_bs4(sami)
-    return _ignore_some_stuff(like_html)
+    like_html = change_tag_for_bs4(sami)
+    return ignore_some_stuff(like_html)
 
 
 def fill_until(multiful_time_series):
---
+++ ureru/helpers.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import io
+import itertools
+import json
+import re
+
+
+change_tag_for_bs4 = lambda s: re.sub(r'(</*)(sync)',
+                                      r'\1td',
+                                      s,
+                                      flags=re.I)
+
+
+ignore_some_stuff = lambda s: re.sub(r'(\s*\-+\s*|<br\s*\/*>|\&nbsp;*)',
+                                     '\n',
+                                     s,
+                                     flags=re.I)
+
+
+def make_sample(file_name, data):
+    with io.open('../utils/{}.js'.format(file_name), 'w', encoding='utf8') as js_file:
+        data = json.dumps(to_dict_for_highchart(data), ensure_ascii=False)
+        js_file.write(u'var {}_data='.format(file_name))
+        js_file.write(data)
+        js_file.write(u';')
+
+
+def to_dict_for_highchart(time_series):
+    def to_dict(accu, caption):
+        return {
+            'x': caption.since,
+            'y': accu,
+            'z': caption.until,
+            'c': caption.sentence.replace('\n', '<br>')
+        }
+    results = []
+    for key, captions in time_series.items():
+        c1, c2 = itertools.tee(captions)
+        accumulates = itertools.accumulate((c.since for c in c1))
+        tmp_dictionary = {'name': key}
+        tmp_dictionary['data'] = [to_dict(accu, caption) for (accu, caption) in zip(accumulates, c2)]
+        results.append(tmp_dictionary)
+    return results
+
+
+def to_string(caption):
+    return '{c.since}>{c.until} : {c.sentence}\n\n'.format(c=caption)
